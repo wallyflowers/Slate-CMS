5b4366567f016d574a498535aa5cabf827994fc2
Wally Flowers <wallyflowers@proton.me>
2024-10-05 17:35:48 -0500

    Initial Commit

---
+++ Slate/SlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlateApp.swift
+//  Slate
+//
+//  Created by MW Giannini on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SlateTests/SlateTests.swift
@@ -0,0 +1,17 @@
+//
+//  SlateTests.swift
+//  SlateTests
+//
+//  Created by MW Giannini on 10/5/24.
+//
+
+import Testing
+@testable import Slate
+
+struct SlateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
